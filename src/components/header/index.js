4e7acf211592aa332850193f1d6fcd69a1817c0e
김범준_집 <kbj0109@gmail.com>
2020-10-04 23:05:42 +0900

    Enger 키에 의한, LocalStorage에 ToDo 입력하기

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default () => {
+  const [topTodo, setTopTodo] = useState("");
+
+  useEffect(() => {
+    const toDoText = localStorage.getItem("toDoList") || "[]";
+    const toDoArray = JSON.parse(toDoText);
+
+    toDoArray.push({ isCompleted: false, title: topTodo });
+    localStorage.setItem("toDoList", JSON.stringify(toDoArray));
+
+    document.getElementById("inputToDo").value = "";
+  }, [topTodo]);
+
+  const saveTodo = (e) => {
+    if (e.key === "Enter") {
+      setTopTodo(e.target.value);
+    }
+  };
+
   return (
     <header className="header">
       <h1>todos</h1>
       <input
+        id="inputToDo"
         className="new-todo"
         placeholder="What needs to be done?"
         autoFocus
-        readOnly
+        onKeyPress={saveTodo}
       />
     </header>
   );
